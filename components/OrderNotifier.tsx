a0eafc598901cd9fcf42403abbc5e9deca564ab1
KMS <kmsdunbiriyani@gmail.com>
2026-07-06 19:37:10 +0530

    Remove browser notifications

--- components/OrderNotifier.tsx
+++ components/OrderNotifier.tsx
@@ -6,38 +6,20 @@ type Props = {
     latestOrderId: number;
 };
 
-export default function OrderNotifier({ latestOrderId }: Props) {
+export default function OrderNotifier({
+    latestOrderId,
+}: Props) {
     const previousId = useRef(latestOrderId);
 
     useEffect(() => {
         if (latestOrderId > previousId.current) {
-
             const audio = new Audio("/sounds/bell.mp3");
-            audio.play().catch(() => {});
 
-            if (
-                typeof window !== "undefined" &&
-                "Notification" in window &&
-                Notification.permission === "granted"
-            ) {
-                new Notification("🍗 New KMS Order", {
-                    body: `Order #${latestOrderId} received`,
-                });
-            }
+            audio.play().catch(() => {});
 
             previousId.current = latestOrderId;
         }
     }, [latestOrderId]);
-
-    useEffect(() => {
-        if (
-            typeof window !== "undefined" &&
-            "Notification" in window &&
-            Notification.permission === "default"
-        ) {
-            Notification.requestPermission().catch(() => {});
-        }
-    }, []);
 
     return null;
 }
